4e00279c4dc0bb7f17cd2b0ca9221a5c40723634
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com>
2024-08-24 01:53:09 +0200

    Complete normal tasks

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const innerContainer = document.querySelector("#innerContainer");
 const changeBoxesButton = document.querySelector("#changeBoxes");
 
 changeBoxesButton.addEventListener("click", () => {getBoxAmount(); changeBoxLayout()});
-
+changeBoxesButton.addEventListener("mouseenter", () => {changeColor(changeBoxesButton, "brown")});
+changeBoxesButton.addEventListener("mouseleave", () => {changeColor(changeBoxesButton, "white")});
+innerContainer.addEventListener("mouseleave", () => changeBoxLayout())
 let BoxAmountHorizontal = 4;
 let BoxAmountVertical = 4;
 
@@ -35,18 +37,36 @@ function getBoxAmount() {
 
 }
 
-function changeBoxLayout() {
+function resetBoxLayout() {
     while (innerContainer.firstChild) {
-      innerContainer.removeChild(innerContainer.firstChild);
-    }
+        innerContainer.removeChild(innerContainer.firstChild);
+      }
+}
+
+function changeBoxLayout() {
+    resetBoxLayout()
     let BoxAmountOverall = BoxAmountHorizontal * BoxAmountVertical;
     for (let i = 0; i < BoxAmountOverall; i++) {
         const box = document.createElement("div");
         box.setAttribute("class", "box");
-        box.setAttribute("style", `width: ${100 / BoxAmountHorizontal}%; border-width: ${50 / BoxAmountOverall}px; border-radius: ${60 / BoxAmountHorizontal}px;`);
+        box.setAttribute("style", `width: ${100 / BoxAmountHorizontal}%; border-radius: ${120 / BoxAmountHorizontal}px`);
         innerContainer.appendChild(box);
+
+        box.addEventListener("mouseenter", () => {
+            changeColor(box, "brown"); changeBorder(box) // Pass the specific box to change its color
+        });
     }
 
+}
+
+//; border-width: ${50 / BoxAmountOverall}px;
+
+function changeColor(element, color) {
+    element.style.backgroundColor = color; // Change this to any color you like
+}
+
+function changeBorder(element) {
+    element.style.borderRadius = "0px";
 }
 
 changeBoxLayout();
