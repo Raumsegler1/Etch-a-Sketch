90b5522b1acf135472a425ca325d0498a1265f94
Raumsegler1 <113139832+Raumsegler1@users.noreply.github.com>
2024-08-23 22:51:15 +0200

    Make the boxes appear with button half working

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 const innerContainer = document.querySelector("#innerContainer");
 const changeBoxesButton = document.querySelector("#changeBoxes");
 
-changeBoxesButton.addEventListener("click", () => {changeBoxLayout()});
+changeBoxesButton.addEventListener("click", () => {getBoxAmount(); changeBoxLayout()});
 
-let BoxAmountHorizontal;
-let BoxAmountVertical;
+let BoxAmountHorizontal = 4;
+let BoxAmountVertical = 4;
+
 function getBoxAmount() {
-    const BoxAmount = prompt("What box layout do you want?", "5 6");
+    const BoxAmount = prompt("What box layout do you want? Nothing over 100", "5 6");
     const BoxAmountArray = BoxAmount.split(" ");
+    if ("number" === typeof Number(BoxAmountArray[0]) && Number(BoxAmountArray[0]) <= 100) {
     BoxAmountHorizontal = Number(BoxAmountArray[0]);
-    if ("number" !== typeof BoxAmountArray[2]) {
-        BoxAmountVertical = Number(BoxAmountArray[0]);
     } else {
-        BoxAmountVertical = Number(BoxAmountArray[2]);
+        BoxAmount = prompt ("Try again", 5)
+    }
+
+
+    if ("number" !== typeof Number(BoxAmountArray[1])) {
+        if ("number" !== typeof Number(BoxAmountArray[2])) {
+            BoxAmountVertical = Number(BoxAmountArray[0]); 
+        } else  if (Number(BoxAmountArray[2]) <= 100){
+            BoxAmountVertical = Number(BoxAmountArray[2]);
+        } else {
+            BoxAmount = prompt ("Try again", 5)
+        }
+    } else  if (Number(BoxAmountArray[1]) <= 100){
+        BoxAmountVertical = Number(BoxAmountArray[1]);
+    } else {
+        BoxAmount = prompt ("Try again", 5)
     }
 
 }
 
 function changeBoxLayout() {
-    innerContainer.replaceChild();
-    getBoxAmount()
+    while (innerContainer.firstChild) {
+      innerContainer.removeChild(innerContainer.firstChild);
+    }
     let BoxAmountOverall = BoxAmountHorizontal * BoxAmountVertical;
-    
+    for (let i = 0; i < BoxAmountOverall; i++) {
+        const box = document.createElement("div");
+        box.setAttribute("class", "box");
+        box.setAttribute("style", `width: ${100 / BoxAmountHorizontal}%; border-width: ${10 / BoxAmountHorizontal}px`);
+        innerContainer.appendChild(box);
+    }
+
 }
+
+changeBoxLayout();
